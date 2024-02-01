6c1e5b7990def4964ac2bd2084bbae9d62cfeadc
thanaism <62784012+thanaism@users.noreply.github.com>
2024-02-01 17:27:45 +0900

    add: Linderaによる形態素解析

--- wasm-example/src/lib.rs
+++ wasm-example/src/lib.rs
@@ -1,7 +1,9 @@
+use lindera::tokenizer::Tokenizer;
+use lindera_core::core::viterbi::Mode;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-extern {
+extern "C" {
     pub fn alert(s: &str);
 }
 
@@ -10,3 +12,12 @@ pub fn greet(name: &str) {
     alert(&format!("Hello, {}!", name));
 }
 
+#[wasm_bindgen]
+pub fn tokenize(x: Option<String>) -> Vec<String> {
+    let mut tokenizer = Tokenizer::new(Mode::Normal, "");
+    tokenizer
+        .tokenize(&x.unwrap())
+        .iter()
+        .map(|x| x.text.into())
+        .collect::<Vec<String>>()
+}
